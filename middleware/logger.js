840d5ee55862937fc44f758ecbce2d1dec0154ca
Philipp Jost <philx_x@hotmail.com>
2016-06-14 16:53:48 -0400

    add requestTime property to req object from middleware

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,6 +1,10 @@
 module.exports = (req, res, next) => {
-  let now = new Date().toUTCString()
+  let now = new Date()
   // of course we could also write this to a file
-  console.log(`${now}: ${req.ip} sent a ${req.method} to ${req.hostname}${req.path}`)
+  console.log(`${now.toUTCString()}: ${req.ip} sent a ${req.method} to ${req.hostname}${req.path}`)
+  // or add things to the req or res objects like this
+  // added properties become available in following route handler and middlewares
+  // makes it extremely easy to use 3rd party middlewares
+  req.requestTime = now
   next()
 }
