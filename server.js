989af16cd3093ea65f87383b1ffee923a704d985
Philipp Jost <philx_x@hotmail.com>
2016-06-14 17:37:20 -0400

    add mustache rendering engine

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express')
+var mustacheExpress = require('mustache-express')
 var app = express()
 
 var logger = require('./middleware/logger')
 app.use(logger)
-
+// Register '.mustache' extension with The Mustache Express
+app.engine('mustache', mustacheExpress())
+app.set('view engine', 'mustache')
+// optional. the default is the views folder in the root directory
+app.set('views', __dirname + '/views')
 
 // register callbacks to routes
 app.get('/', (req, res) => {
@@ -29,16 +34,30 @@ app.get('/rc-*', (req, res) => {
   res.send(`sucessfully routed to ${path}`)
 })
 
-app.get('/user/:user_id', (req,res) => {
+app.get('/api/user/:user_id', (req, res) => {
   let userId = req.params.user_id
   // make some db query for userId here
   let user = {
     id: userId,
     name: "phil",
+    skills: ["express", "node", "mustache"]
   }
   // send JSON response
   res.json(user)
 })
+
+app.get('/user/:user_id', (req, res) => {
+  let userId = req.params.user_id
+  let user = {
+    id: userId,
+    name: "phil",
+    skills: ["express", "node", "mustache"]
+  }
+  // Mustache.render() gets called under the hood
+  // which takes the name of the view and data to render
+  res.render('user', user)
+})
+
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
